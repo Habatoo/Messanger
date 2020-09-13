1c653e71582aec8bf9ecf0d11c316e27c6a64d2a
Habatoo <habatoo@gmail.com>
2020-09-13 23:13:31 +0700

    add received and send text view add button

--- app/src/main/java/demo/com/ReceivedMessageActivity.java
+++ app/src/main/java/demo/com/ReceivedMessageActivity.java
@@ -3,16 +3,12 @@ package demo.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ReceivedMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void onClickSendMessage(View view) {
+        setContentView(R.layout.activity_received_message);
     }
 }
