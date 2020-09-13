a128bb82ced55aeaad5ee685b02669b9f6e760fa
Habatoo <habatoo@gmail.com>
2020-09-13 23:33:11 +0700

    add send message to another activity

--- app/src/main/java/demo/com/MainActivity.java
+++ app/src/main/java/demo/com/MainActivity.java
@@ -2,17 +2,26 @@ package demo.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editTextTextMessage);
     }
 
     public void onClickSendMessage(View view) {
+        String msg = editText.getText().toString();
+        Intent intent = new Intent(this, ReceivedMessageActivity.class);
+        intent.putExtra("msg", msg);
+        startActivity(intent);
     }
 }
